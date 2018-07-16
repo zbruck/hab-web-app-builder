233ecc1c0837e22f3dd30c742864d597f5b5ce13
Zachary Bruck <zbruck.zb@gmail.com>
2018-07-16 18:11:15 -0400

    Use my Imagemagick package

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=habitatize
 pkg_origin=zbruck
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.1.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.2.0"
+pkg_deps=( zbruck/imagemagick )
